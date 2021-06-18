4ca0ebb0d9946fe45b5e7e90e63b3bac7a965b25
oilcake <oilpie@gmail.com>
2021-06-18 04:41:39 +0300

    now accepts command line arguments

--- README.md
+++ README.md
@@ -7,14 +7,21 @@ Solves 1000000 sudokus
 __Usage__
 
 ```
-python main.py
+usage:
+main.py [-h] sudokus
+
+positional arguments:
+  sudokus     .csv with unsolved sudokus
+
+optional arguments:
+  -h, --help  show this help message and exit
 ```
 
 [download 1000000 sudokus](https://drive.google.com/file/d/1tH7WqOxoG-9k5lRZlEU4HM_mHh10_HlD/view?usp=sharing)
 
-__Sudoku_reader__ - reads csv file with sudoku_examples
+__Sudoku_reader__ - reads csv file with sudoku examples
 
-__Matrix__ - builds a matrix with task
+__Matrix__ - builds a matrix from task
 
 __Solver__ - solves task
 
--- main.py
+++ main.py
@@ -4,28 +4,45 @@ from checker import *
 from solver import *
 import argparse
 
-parser = argparse.ArgumentParser(description='give me some sudokus')
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
-FILE = 'sudoku_examples.csv'
+parser = argparse.ArgumentParser(description='give me some sudokus')
+parser.add_argument('sudokus', type=str, help='.csv with unsolved sudokus')
+args = parser.parse_args()
+
+answer_1 = input('Do you want to see matrixes?(yes/no)')
+to_print = str2bool(answer_1)
+
+answer_2 = input('One at a time?(yes/no)')
+one = str2bool(answer_2)
 
 solved_count = 0
 unsolved_count = 0
 
-reader = SudokuReader(FILE)
+reader = SudokuReader(args.sudokus)
 sudokus = reader.read() # returns a dict with 'quizzes' and 'solutions'
 
 for sudoku in sudokus:
     task = Matrix(sudoku['quizzes'])
     solution = Matrix(sudoku['solutions'])
-    print('Task')
-    task.show()
-    print('Solution')
-    solution.show()
+    if to_print:
+        print('Task')
+        task.show()
+        print('Solution')
+        solution.show()
     solver = Solver(task.matrix)
     task.matrix = solver.solve()
-
-    print('Task solved')
-    task.show()
+    if to_print:
+        print('Task solved')
+        task.show()
     checker = Checker(task.matrix, solution.matrix)
 
     if checker.solved():
@@ -35,5 +52,6 @@ for sudoku in sudokus:
 
     print('correct', solved_count)
     print('incorrect', unsolved_count)
-    input('Next?')
+    if one:
+        input('hit any key to process next one')
 reader.close()
